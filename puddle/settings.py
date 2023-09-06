a8ee842821d6753b2687f4da535bf2e4d65f8333
Shivam <shivamv1332@gmail.com>
2023-09-06 10:24:46 +0530

    changed the MEDIA_ROOT in settings.py to not include test uploads to the github repo and removed the previously include media folder

--- puddle/settings.py
+++ puddle/settings.py
@@ -124,8 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR.parent / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
